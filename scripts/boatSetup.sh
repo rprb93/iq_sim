2b9f25dab319ac8abb26f79d8eac60874d971fbd
Rui Baptista <rprb93@gmail.com>
2021-12-13 14:33:08 +0000

    Hugo changes

--- scripts/boatSetup.sh
+++ scripts/boatSetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-if cat ~/ardupilot/Tools/autotest/locations.txt | grep "Viridian=32.794229,-97.094320,143,0"
+if cat ~/ardupilot/Tools/autotest/locations.txt | grep "Viridian=32.794229,-97.094320,0.0,0"
 then
 echo "location exist already"
 else
-echo "Viridian=32.794229,-97.094320,143,0" >> ~/ardupilot/Tools/autotest/locations.txt
+echo "Viridian=32.794229,-97.094320,0.0,0" >> ~/ardupilot/Tools/autotest/locations.txt
 fi
