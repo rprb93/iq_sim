3f75567c5d046addfd4298b1b3d85cb25b024451
Rui Baptista <rprb93@gmail.com>
2022-03-22 17:33:12 +0000

    hm version at 22/03/22

--- scripts/startsitl.sh
+++ scripts/startsitl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ~/RepArdupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
